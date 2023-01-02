29ff56e02eadd839ab5791294fe8c824e5509d18
Wojciech Skrzyński <skrzynia777@gmail.com>
2023-01-02 23:53:31 +0000

    Android app update #1
    +: Mqqt connection, Text fields, Tokens

--- src/main/java/com/example/plantio/MQTTconnector.java
+++ src/main/java/com/example/plantio/MQTTconnector.java
@@ -10,52 +10,89 @@ import android.os.Build;
 
 public class MQTTconnector {
 
-    final String host = "039647b9e22e4235953dd482038220c7.s2.eu.hivemq.cloud";
-    final String username = "skrzynia";
-    final String password = "9RB#kx!Hgc33Hv#";
+    private final String host = "039647b9e22e4235953dd482038220c7.s2.eu.hivemq.cloud";
+    private final String username = "skrzynia";
+    private final String password = "9RB#kx!Hgc33Hv#";
+    private static StringBuilder temp = new StringBuilder("0");
+    private static StringBuilder humidity = new StringBuilder("0");
+    private static StringBuilder pressure = new StringBuilder("0");
+    MQTTconnector() {
 
-    MQTTconnector() {}
+    }
 
-     {
-         final Mqtt5BlockingClient client = MqttClient.builder()
-                 .useMqttVersion5()
-                 .serverHost(host)
-                 .serverPort(8883)
-                 .sslWithDefaultConfig()
-                 .buildBlocking();
+    public StringBuilder getTemp() {
+        return temp;
+    }
 
-         // connect to HiveMQ Cloud with TLS and username/pw
-         client.connectWith()
-                 .simpleAuth()
-                 .username(username)
-                 .password(UTF_8.encode(password))
-                 .applySimpleAuth()
-                 .send();
+    public void setTemp(StringBuilder temp) {
+        this.temp = temp;
+    }
 
-         System.out.println("Connected successfully");
+    public StringBuilder getHumidity() {
+        return humidity;
+    }
 
-         // subscribe to the topic "my/test/topic"
-         client.subscribeWith()
-                 .topicFilter("weather")
-                 .send();
+    public void setHumidity(StringBuilder humidity) {
+        this.humidity = humidity;
+    }
 
-         // set a callback that is called when a message is received (using the async API style)
-         client.toAsync().publishes(ALL, publish -> {
-             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                 System.out.println("Received message: " +
-                         publish.getTopic() + " -> " +
-                         UTF_8.decode(publish.getPayload().get()));
-             }
+    public StringBuilder getPressure() {
+        return pressure;
+    }
 
-             // disconnect the client after a message was received
-             client.disconnect();
-         });
+    public void setPressure(StringBuilder pressure) {
+        this.pressure = pressure;
+    }
 
-         // publish a message to the topic "my/test/topic"
-         client.publishWith()
-                 .topic("my/test/topic")
-                 .payload(UTF_8.encode("Hello"))
-                 .send();
+    private String[] getTokens(String text)
+    {
+        return text.split("\\R");
+    }
+
+    {
+
+        final Mqtt5BlockingClient  client = MqttClient.builder()
+                .useMqttVersion5()
+                .serverHost(host)
+                .serverPort(8883)
+                .sslWithDefaultConfig()
+                .buildBlocking();
+
+        // connect to HiveMQ Cloud with TLS and username/pw
+        client.connectWith()
+                .simpleAuth()
+                .username(username)
+                .password(UTF_8.encode(password))
+                .applySimpleAuth()
+                .send();
+
+        System.out.println("Connected successfully");
+
+        client.toAsync().publishes(ALL, publish -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                 System.out.println("Received message: " +
+//                         publish.getTopic() + " -> " +
+//                         UTF_8.decode(publish.getPayload().get()));
+                String tokens[] = getTokens(String.valueOf(UTF_8.decode(publish.getPayload().get())));
+
+                setTemp(new StringBuilder(tokens[0]));
+                setHumidity(new StringBuilder(tokens[1]));
+                setPressure(new StringBuilder(tokens[2]));
+            }
+
+            // disconnect the client after a message was received
+            client.disconnect();
+        });
+
+
+        client.subscribeWith()
+                .topicFilter("weather")
+                .send();
+
+        client.publishWith()
+                .topic("my/test/topic")
+                .payload(UTF_8.encode("Hello"))
+                .send();
 
     }
 
--- src/main/java/com/example/plantio/MainActivity.java
+++ src/main/java/com/example/plantio/MainActivity.java
@@ -1,18 +1,18 @@
 package com.example.plantio;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    SSHClass sshClass = null;
+
+    TextView temp;
+    TextView humidity;
+    TextView pressure;
 
 
     @Override
@@ -23,27 +23,50 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView = (WebView) findViewById(R.id.web);
         WebSettings webSettings = webView.getSettings();
+
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
         webView.loadUrl("http://192.168.0.200:8080/?action=stream");
 
+        temp = (TextView) findViewById(R.id.textView);
+        humidity = (TextView) findViewById(R.id.textView2);
+        pressure = (TextView) findViewById(R.id.textView3);
 
-        WebView webView1 = (WebView) findViewById(R.id.wv);
-        webView1.loadUrl("https://thingspeak.com/channels/1986256/widgets/575692");
 
-        while(true)
+
+
+        Thread t = new Thread()
         {
-            MQTTconnector tconnector = new MQTTconnector();
-            try {
-                Thread.sleep(15000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            @Override
+            public void run()
+            {
+                while (!isInterrupted())
+                {
+                    try {
+                        Thread.sleep(1500);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                MQTTconnector tconnector = new MQTTconnector();
+                                temp.setText(tconnector.getTemp());
+                                humidity.setText(tconnector.getHumidity());
+                                pressure.setText(tconnector.getPressure());
+                                System.out.println(tconnector.getTemp() + " " + tconnector.getHumidity() + " " + tconnector.getPressure());
+                            }
+                        });
+                    }catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
+        };
+
+        t.start();
+
 
     }
-
-
-
 
 
 }
